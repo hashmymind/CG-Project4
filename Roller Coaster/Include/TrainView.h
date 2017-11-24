123b1db047df6b55bb6fff05df948ad2cccb8c98
qwe321qwe321qwe321 <aegis321123@gmail.com>
2017-11-24 21:35:49 +0800

    Revert to origin ning

---
+++ Roller Coaster/Include/TrainView.h
@@ -0,0 +1,64 @@
+#ifndef TRAINVIEW_H  
+#define TRAINVIEW_H  
+#include <QtOpenGL/QGLWidget> 
+#include <QtGui/QtGui>  
+#include <QtOpenGL/QtOpenGL>  
+#include <GL/GLU.h>
+#include <QtGui/QOpenGLFunctions_4_3_Core>
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib") 
+#include "Utilities/ArcBallCam.H"
+#include "Utilities/3DUtils.H"
+#include "Track.H"
+
+class AppMain;
+class CTrack;
+
+//#######################################################################
+// TODO
+// You might change the TrainView in order to add different objects to
+// be drawn, or adjust the lighting, or ...
+//#######################################################################
+
+
+class TrainView : public QGLWidget, protected QOpenGLFunctions_4_3_Core
+{  
+	Q_OBJECT  
+public:  
+	explicit TrainView(QWidget *parent = 0);  
+	~TrainView();  
+
+public:
+	// overrides of important window things
+	//virtual int handle(int);
+	virtual void paintGL();
+
+	// all of the actual drawing happens in this routine
+	// it has to be encapsulated, since we draw differently if
+	// we're drawing shadows (no colors, for example)
+	void drawStuff(bool doingShadows=false);
+
+	// setup the projection - assuming that the projection stack has been
+	// cleared for you
+	void setProjection();
+
+	// Reset the Arc ball control
+	void resetArcball();
+
+	// pick a point (for when the mouse goes down)
+	void doPick(int mx, int my);
+
+	void initializeGL();
+
+public:
+	ArcBallCam		arcball;			// keep an ArcBall for the UI
+	int				selectedCube;  // simple - just remember which cube is selected
+
+	CTrack*			m_pTrack;		// The track of the entire scene
+
+	int camera;
+	int curve;
+	int track;
+	bool isrun;
+};  
+#endif // TRAINVIEW_H  
